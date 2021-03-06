56dbc4c851a66a18c637acb10fd5b66eb75ffb93
André Luiz Miotto <miotto.andre77@gmail.com>
2021-03-06 00:21:01 -0300

    Update module-info.java
    Atualiza module-info.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module br.com.fleetmanager {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
+    opens br.com.fleetmanager.model to javafx.base;
     opens br.com.fleetmanager.controller to javafx.fxml;
     exports br.com.fleetmanager;
 }
