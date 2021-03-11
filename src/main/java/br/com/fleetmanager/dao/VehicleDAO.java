9587f21ff8eaeb3acca1059b41027f40f74d30ca
André Luiz Miotto <miotto.andre77@gmail.com>
2021-03-10 23:36:14 -0300

    Update VehicleDAO.java
    Implementa método Insert e método Update.

--- src/main/java/br/com/fleetmanager/dao/VehicleDAO.java
+++ src/main/java/br/com/fleetmanager/dao/VehicleDAO.java
@@ -3,10 +3,7 @@ package br.com.fleetmanager.dao;
 import br.com.fleetmanager.abstracts.ABaseDAO;
 import br.com.fleetmanager.model.Vehicle;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,8 +14,10 @@ public class VehicleDAO extends ABaseDAO {
     }
 
     @Override
-    public boolean Insert(Object object) {
-        return false;
+    public boolean Save(Object object) {
+        if (((Vehicle) object).getId() > 0)
+            return Update(object);
+        return Insert(object);
     }
 
     @Override
@@ -27,15 +26,45 @@ public class VehicleDAO extends ABaseDAO {
     }
 
     @Override
+    public boolean Insert(Object object) {
+        String sql = ("INSERT INTO VEICULO (placa, descricao, anoFabricacao) VALUES (?, ?, ?)");
+        try(PreparedStatement pStmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            pStmt.setString(1, ((Vehicle) object).getPlate());
+            pStmt.setString(2, ((Vehicle) object).getDescription());
+            pStmt.setString(3, ((Vehicle) object).getYearFabricated());
+            pStmt.execute();
+
+            return true;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
     public boolean Update(Object object) {
+        String sql = ("UPDATE VEICULO SET placa = ?, descricao = ?, anoFabricacao = ? WHERE id = ?");
+        try(PreparedStatement pStmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            pStmt.setString(1, ((Vehicle) object).getPlate());
+            pStmt.setString(2, ((Vehicle) object).getDescription());
+            pStmt.setString(3, ((Vehicle) object).getYearFabricated());
+            pStmt.setInt(4, ((Vehicle) object).getId());
+            pStmt.execute();
+
+            return true;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return false;
     }
 
     @Override
     public List ListAll() throws SQLException {
 
-        List<Vehicle> vehicles = new ArrayList<Vehicle>();
-        String sql = ("SELECT ID, PLACA, DESCRICAO, ANOFABRICACAO FROM VEICULO");
+        List<Vehicle> vehicles = new ArrayList<>();
+        String sql = ("SELECT * FROM VEICULO ORDER BY id ASC");
 
         try(PreparedStatement pStmt = connection.prepareStatement(sql)) {
             pStmt.execute();
@@ -46,7 +75,7 @@ public class VehicleDAO extends ABaseDAO {
                             rSet.getInt(1),
                             rSet.getString(2),
                             rSet.getString(3),
-                            rSet.getInt(4));
+                            rSet.getString(4));
                     vehicles.add(vehicle);
                 }
             }
