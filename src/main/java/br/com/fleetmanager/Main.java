dc25f1d1af2ff4a64b7b4b0df0711c926ddaa000
André Luiz Miotto <miotto.andre77@gmail.com>
2021-03-10 23:33:54 -0300

    Update Main.java
    Implementa método para obter Stage principal da aplicação.

--- src/main/java/br/com/fleetmanager/Main.java
+++ src/main/java/br/com/fleetmanager/Main.java
@@ -1,19 +1,31 @@
 package br.com.fleetmanager;
 
 import br.com.fleetmanager.controller.WindowController;
+import br.com.fleetmanager.utils.FXMLEnum;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static Stage stage;
+
     @Override
     public void start(Stage pStage) throws Exception{
-        WindowController.openWindow("mainWindow", pStage);
+        setPrimaryStage(pStage);
+        WindowController.openWindow(FXMLEnum.MAINWINDOW, pStage);
     }
 
     public static void main(String[] args) {
 
         launch(args);
+    }
+
+    public static Stage getPrimaryStage() {
+        return stage;
+    }
+
+    private void setPrimaryStage(Stage pStage) {
+        Main.stage = pStage;
     }
 
 }
