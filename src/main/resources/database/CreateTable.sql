0c276f5461840403ee9bb227e1ec2472eec010da
André Luiz Miotto <miotto.andre77@gmail.com>
2021-03-16 22:30:10 -0300

    Update CreateTable.sql
    Adiciona script de criação da tabela de categorias financeiras.

--- src/main/resources/database/CreateTable.sql
+++ src/main/resources/database/CreateTable.sql
@@ -1,12 +1,32 @@
 CREATE TABLE public.veiculo
 (
 	id SERIAL NOT NULL,
+	status smallint NOT NULL DEFAULT 1,
     placa VARCHAR(10),
     descricao VARCHAR(100),
-    anofabricacao VARCHAR(5)
+    anofabricacao VARCHAR(5),
+    PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
 
 ALTER TABLE public.veiculo
     OWNER to postgres;
+
+CREATE TABLE public.categoriafinanceira
+(
+    id SERIAL NOT NULL,
+    status smallint NOT NULL DEFAULT 1,
+    descricao VARCHAR(50),
+    tipo smallint NOT NULL,
+    PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.categoriafinanceira
+    OWNER to postgres;
+
+COMMENT ON COLUMN public.categoriafinanceira.tipo
+    IS '1 - Receita
+        2 - Despesa';
