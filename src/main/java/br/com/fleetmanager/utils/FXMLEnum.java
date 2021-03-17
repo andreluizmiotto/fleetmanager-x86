2c1573e0fe8a39f720863f56844418e28e7f5403
André Luiz Miotto <miotto.andre77@gmail.com>
2021-03-16 22:37:51 -0300

    Renomeia classe de Enum

--- src/main/java/br/com/fleetmanager/utils/FXMLEnum.java
+++ src/main/java/br/com/fleetmanager/utils/FXMLEnum.java
@@ -1,5 +1,29 @@
 package br.com.fleetmanager.utils;
 
-public enum FXMLEnum {
-    MAINWINDOW, LISTVEHICLE
+import java.util.AbstractMap;
+import java.util.HashMap;
+
+public class FXMLEnum {
+
+    public static enum Enum {
+        MAINWINDOW, VEHICLE, FINANCIALCATEGORY
+    }
+
+    private static final HashMap<FXMLEnum.Enum, AbstractMap.SimpleEntry<String, String>> FXMLMapEnum = new HashMap<>();
+
+    static {
+        FXMLMapEnum.put(Enum.MAINWINDOW, new AbstractMap.SimpleEntry<>("mainWindow", "Gerenciador de Frota"));
+        FXMLMapEnum.put(Enum.VEHICLE, new AbstractMap.SimpleEntry<>("vehicle", "Veículos"));
+        FXMLMapEnum.put(Enum.FINANCIALCATEGORY, new AbstractMap.SimpleEntry<>("financialCategory", "Categorias financeiras"));
+    }
+
+    public static String getFXMLFile(FXMLEnum.Enum pFXML) {
+        return FXMLMapEnum.get(pFXML).getKey();
+    }
+
+    public static String getFXMLTittle(FXMLEnum.Enum pFXML) {
+        return FXMLMapEnum.get(pFXML).getValue();
+    }
+
+
 }
