94dc9102304a1f1e3dd08432492b55e573a3b958
André Luiz Miotto <miotto.andre77@gmail.com>
2021-03-16 22:31:58 -0300

    Update MainController.java
    Implementa Initializable para setar actions dos menus.

--- src/main/java/br/com/fleetmanager/controller/MainController.java
+++ src/main/java/br/com/fleetmanager/controller/MainController.java
@@ -1,21 +1,44 @@
 package br.com.fleetmanager.controller;
 
 import br.com.fleetmanager.utils.FXMLEnum;
-import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.MenuItem;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainController {
+public class MainController implements Initializable {
 
     @FXML
-    private MenuItem menuItemCadVeiculos;
+    private MenuItem menuItemVehicles;
 
     @FXML
-    void actionOpenCadastroVeiculos(ActionEvent event) throws IOException {
-        WindowController.openWindow(FXMLEnum.LISTVEHICLE);
+    private MenuItem menuItemCategories;
+
+    EventHandler<ActionEvent> actionOpenVehicles = (ActionEvent event) -> {
+        try {
+            WindowController.openWindow(FXMLEnum.Enum.VEHICLE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    };
+
+    EventHandler<ActionEvent> actionOpenCategories = (ActionEvent event) -> {
+        try {
+            WindowController.openWindow(FXMLEnum.Enum.FINANCIALCATEGORY);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    };
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        menuItemVehicles.setOnAction(actionOpenVehicles);
+        menuItemCategories.setOnAction(actionOpenCategories);
     }
 
 }
