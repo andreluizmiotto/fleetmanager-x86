e754cce6e5f34ae4a22b2a5b947ef965530eeafb
André Luiz Miotto <miotto.andre77@gmail.com>
2021-04-14 23:45:43 -0300

    Update Functions.java
    Implementa funções de formatação. Implementa função para buscar uma ImageView do diretório.

--- src/main/java/br/com/fleetmanager/utils/Functions.java
+++ src/main/java/br/com/fleetmanager/utils/Functions.java
@@ -1,9 +1,25 @@
 package br.com.fleetmanager.utils;
 
+import javafx.scene.image.ImageView;
+
 public class Functions {
 
     public static boolean isNull(String value) {
         return ((value == null) || (value.isBlank()));
+    }
+
+    public static String formatMonetary(final String pValue, final int pDigit) {
+        return String.valueOf(formatMonetary(Double.parseDouble(pValue), pDigit));
+    }
+
+    public static double formatMonetary(final double pValue, final int digit) {
+        double result = pValue * 10;
+        result += digit * 0.01;
+        return result;
+    }
+
+    public static ImageView getImageView(Class<?> pClass, String pImageName) {
+        return new ImageView(pClass.getResource(Constants.sIconsFolder) + pImageName);
     }
 
 }
