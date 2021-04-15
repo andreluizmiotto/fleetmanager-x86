e7361ae24b71470182e06aa2e84d539849b73b75
André Luiz Miotto <miotto.andre77@gmail.com>
2021-04-14 23:39:05 -0300

    Create FXMLStaticFunctions.java
    Implementa classe de funções estáticas de validação de componentes JavaFX.

---
+++ src/main/java/br/com/fleetmanager/utils/FXMLStaticFunctions.java
@@ -0,0 +1,35 @@
+package br.com.fleetmanager.utils;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class FXMLStaticFunctions {
+
+    private static final String cCssErrorFlag = "error";
+
+    public static void clearErrorClass(TextField pTField) {
+        if (!pTField.getText().isBlank() && pTField.getStyleClass().contains(cCssErrorFlag))
+            pTField.getStyleClass().remove(cCssErrorFlag);
+    }
+
+    public static void clearErrorClass(ComboBox pComboBox) {
+        if ((pComboBox.getValue() != null) && pComboBox.getStyleClass().contains(cCssErrorFlag))
+            pComboBox.getStyleClass().remove(cCssErrorFlag);
+    }
+
+    public static boolean validateField(TextField pTField) {
+        if (pTField.getText().isBlank()) {
+            pTField.getStyleClass().add("error");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validateField(ComboBox pComboBox) {
+        if (pComboBox.getValue() != null)
+            return true;
+        pComboBox.getStyleClass().add("error");
+        return false;
+    }
+
+}
