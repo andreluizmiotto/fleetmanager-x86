20391e86c3bf9bba000e381e1225d14d0c5db133
André Luiz Miotto <miotto.andre77@gmail.com>
2021-04-14 23:44:57 -0300

    Update FXMLEnum.java
    Adiciona enum da rotina de lançamentos financeiros. Adiciona Enums de ícones das rotinas do sistema.

--- src/main/java/br/com/fleetmanager/utils/FXMLEnum.java
+++ src/main/java/br/com/fleetmanager/utils/FXMLEnum.java
@@ -6,15 +6,15 @@ import java.util.HashMap;
 public class FXMLEnum {
 
     public static enum Enum {
-        MAINWINDOW, VEHICLE, FINANCIALCATEGORY
+        MAINWINDOW, VEHICLE, FINANCIALCATEGORY, FINANCIALTRANSACTION
     }
 
     private static final HashMap<FXMLEnum.Enum, AbstractMap.SimpleEntry<String, String>> FXMLMapEnum = new HashMap<>();
-
     static {
         FXMLMapEnum.put(Enum.MAINWINDOW, new AbstractMap.SimpleEntry<>("mainWindow", "Gerenciador de Frota"));
         FXMLMapEnum.put(Enum.VEHICLE, new AbstractMap.SimpleEntry<>("vehicle", "Veículos"));
         FXMLMapEnum.put(Enum.FINANCIALCATEGORY, new AbstractMap.SimpleEntry<>("financialCategory", "Categorias financeiras"));
+        FXMLMapEnum.put(Enum.FINANCIALTRANSACTION, new AbstractMap.SimpleEntry<>("financialTransactions", "Lançamentos financeiros"));
     }
 
     public static String getFXMLFile(FXMLEnum.Enum pFXML) {
@@ -25,5 +25,16 @@ public class FXMLEnum {
         return FXMLMapEnum.get(pFXML).getValue();
     }
 
+    private static final HashMap<FXMLEnum.Enum, String> FXMLIconEnum = new HashMap<>();
+    static {
+        FXMLIconEnum.put(Enum.MAINWINDOW, "semi_truck_16px");
+        FXMLIconEnum.put(Enum.VEHICLE, "truck_16px");
+        FXMLIconEnum.put(Enum.FINANCIALCATEGORY, "cashbook_16px");
+        FXMLIconEnum.put(Enum.FINANCIALTRANSACTION, "transaction_16px");
+    }
+
+    public static String getFXMLIcon(FXMLEnum.Enum pFXML) {
+        return FXMLIconEnum.get(pFXML) + ".png";
+    }
 
 }
