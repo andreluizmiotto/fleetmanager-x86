a4e3bc99693c21f632809d588449bd25d133d565
André Luiz Miotto <miotto.andre77@gmail.com>
2021-04-14 23:30:52 -0300

    Update CreateTable.sql
    Adiciona comandos de criação da tabela de lançamentos financeiros.

--- src/main/resources/database/CreateTable.sql
+++ src/main/resources/database/CreateTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE public.veiculo
 (
-	id SERIAL NOT NULL,
+	id serial NOT NULL,
 	status smallint NOT NULL DEFAULT 1,
     placa VARCHAR(10),
     descricao VARCHAR(100),
@@ -15,7 +15,7 @@ ALTER TABLE public.veiculo
 
 CREATE TABLE public.categoriafinanceira
 (
-    id SERIAL NOT NULL,
+    id serial NOT NULL,
     status smallint NOT NULL DEFAULT 1,
     descricao VARCHAR(50),
     tipo smallint NOT NULL,
@@ -30,3 +30,41 @@ ALTER TABLE public.categoriafinanceira
 COMMENT ON COLUMN public.categoriafinanceira.tipo
     IS '1 - Receita
         2 - Despesa';
+
+CREATE TABLE public.lancfinanceiro
+(
+    id serial NOT NULL,
+    status smallint NOT NULL DEFAULT 1,
+    data date NOT NULL,
+    idveiculo integer NOT NULL,
+    idcategoria integer NOT NULL,
+    valor double precision NOT NULL,
+    descricao VARCHAR(100),
+    PRIMARY KEY (id),
+    CONSTRAINT fk_veiculo FOREIGN KEY (idveiculo)
+        REFERENCES public.veiculo (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT fk_categoria FOREIGN KEY (idcategoria)
+        REFERENCES public.categoriafinanceira (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
+
+ALTER TABLE public.lancfinanceiro
+    OWNER to postgres;
+
+CREATE TABLE public.auditoria
+(
+    id serial NOT NULL,
+    status smallint NOT NULL DEFAULT 1,
+    tabela VARCHAR(50) NOT NULL,
+    idRegistro integer NOT NULL,
+    data timestamp without time zone NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE public.auditoria
+    OWNER to postgres;
