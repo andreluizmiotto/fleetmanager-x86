f65f58d2ae234fe7fea4469654cc6bc485442463
André Luiz Miotto <miotto.andre77@gmail.com>
2021-04-14 23:33:47 -0300

    Implementa Auditoria de transações SQL

--- src/main/java/br/com/fleetmanager/abstracts/ABaseDAO.java
+++ src/main/java/br/com/fleetmanager/abstracts/ABaseDAO.java
@@ -1,21 +1,68 @@
 package br.com.fleetmanager.abstracts;
 
 import br.com.fleetmanager.interfaces.IBaseDAO;
+import br.com.fleetmanager.utils.Constants;
 
-import java.sql.Connection;
+import java.sql.*;
 
 public abstract class ABaseDAO implements IBaseDAO {
 
     protected Connection connection;
+    private String tableName;
 
-    public ABaseDAO(Connection pConnection) {
+    public ABaseDAO(Connection pConnection, String pTableName) {
         this.connection = pConnection;
+        this.tableName = pTableName;
     }
 
-    public ABaseDAO() {
+    public ABaseDAO(String pTableName) {
+        this.tableName = pTableName;
     }
 
     public void setConnection(Connection connection) {
         this.connection = connection;
+    }
+
+    public boolean Delete(int id) {
+        String sql = ("UPDATE " + tableName + " SET status = ? WHERE id = ?");
+        try(PreparedStatement pStmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            pStmt.setShort(1, Constants.cStatusInactive);
+            pStmt.setInt(2, id);
+            pStmt.execute();
+            Audit(id);
+
+            return true;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return false;
+    }
+
+    public void Audit(int id) {
+        String sql = ("INSERT INTO auditoria (tabela, idRegistro, data) VALUES (?, ?, ?)");
+        try(PreparedStatement pStmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            pStmt.setString(1, tableName);
+            pStmt.setInt(2, id);
+            pStmt.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+            pStmt.execute();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void Execute(PreparedStatement pStmt) throws SQLException {
+        pStmt.execute();
+        try (ResultSet keys = pStmt.getGeneratedKeys()) {
+            if (keys.next())
+                Audit(keys.getInt(1));
+        }
+    }
+
+    public void Execute(PreparedStatement pStmt, int pId) throws SQLException {
+        pStmt.execute();
+        Audit(pId);
     }
 }
--- src/main/java/br/com/fleetmanager/interfaces/IBaseDAO.java
+++ src/main/java/br/com/fleetmanager/interfaces/IBaseDAO.java
@@ -1,14 +1,15 @@
 package br.com.fleetmanager.interfaces;
 
-import java.sql.SQLException;
+import java.sql.Date;
 import java.util.List;
 
 public interface IBaseDAO<T> {
 
-    boolean Save(T Object);
-    boolean Delete(int id);
-    List<T> ListAll() throws SQLException;
+    boolean Save(T object);
+    List<T> ListAll();
+    List<T> ListByPeriod(Date dtIni, Date dtFin);
+    T Find(int id);
 
-    boolean Insert(T Object);
-    boolean Update(T Object);
+    boolean Insert(T object);
+    boolean Update(T object);
 }
