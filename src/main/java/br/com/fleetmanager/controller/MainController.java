9e039918c1320cbbed7fa0a014efec64bbec7306
André Luiz Miotto <miotto.andre77@gmail.com>
2021-04-25 22:37:46 -0300

    feat: implement filters to reports and implement analytic historic report

--- src/main/java/br/com/fleetmanager/controller/MainController.java
+++ src/main/java/br/com/fleetmanager/controller/MainController.java
@@ -6,6 +6,7 @@ import br.com.fleetmanager.model.Vehicle;
 import br.com.fleetmanager.utils.AutoCompleteCombobox;
 import br.com.fleetmanager.utils.FXMLEnum;
 import br.com.fleetmanager.utils.Functions;
+import javafx.beans.InvalidationListener;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -57,6 +58,12 @@ public class MainController implements Initializable {
     private DatePicker dtFinalDate;
 
     @FXML
+    private CheckBox ckbSinceTheBeginning;
+
+    @FXML
+    private CheckBox ckbSelectVehicle;
+
+    @FXML
     private RadioButton rbSimplReport;
 
     @FXML
@@ -70,9 +77,6 @@ public class MainController implements Initializable {
 
     @FXML
     private CheckBox ckbGroupByCategory;
-
-    @FXML
-    private Button btnGenerateHistoric;
 
     final EventHandler<ActionEvent> actionOpenVehicles = (ActionEvent event) -> {
         try {
@@ -98,34 +102,50 @@ public class MainController implements Initializable {
         }
     };
 
-    final EventHandler<ActionEvent> actionGenerateReport = (ActionEvent event) -> {
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("dataInicial", Date.from(dtInitialDate.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
-        parameters.put("dataFinal", Date.from(dtFinalDate.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
-
-        String reportName = "analyticReport";
-        if (rbSimplReport.isSelected())
-            reportName = "syntheticReport";
-
-        ReportController report = new ReportController(parameters, reportName);
-        report.GeneratePDF();
+    final EventHandler<ActionEvent> actionEnableCbVehicle = (ActionEvent event) -> {
+        cbVehicleHist.setDisable(!ckbSelectVehicle.isSelected());
     };
 
-    final EventHandler<ActionEvent> actionGenerateHistoric = (ActionEvent event) -> {
+    final EventHandler<ActionEvent> actionEnablePeriod = (ActionEvent event) -> {
+        dtInitialDate.setDisable(ckbSinceTheBeginning.isSelected());
+        dtFinalDate.setDisable(ckbSinceTheBeginning.isSelected());
+    };
 
-        if (isRequiredFieldMissing(cbVehicleHist))
-            return;
+    final EventHandler<ActionEvent> actionGenerateReport = (ActionEvent event) -> {
 
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("sqlFieldExtra", "idVeiculo");
-        parameters.put("sqlValorExtra", cbVehicleHist.getValue().getId());
+        btnGenerateReport.setDisable(true);
+        try {
+            if (ckbSelectVehicle.isSelected() && (isRequiredFieldMissing(cbVehicleHist)))
+                return;
 
-        String reportName = "analyticReport";
-        if (ckbGroupByCategory.isSelected())
-            reportName = "analyticReport";
+            Map<String, Object> parameters = new HashMap<>();
+            if (ckbSinceTheBeginning.isSelected()) {
+                parameters.put("dataInicial", Date.from(LocalDate.of(2000, 1, 1).atStartOfDay(ZoneId.systemDefault()).toInstant()));
+                parameters.put("dataFinal", Date.from(LocalDate.of(2100, 12, 30).atStartOfDay(ZoneId.systemDefault()).toInstant()));
+                parameters.put("semPeriodo", true);
+            } else {
+                parameters.put("dataInicial", Date.from(dtInitialDate.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
+                parameters.put("dataFinal", Date.from(dtFinalDate.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
+                parameters.put("semPeriodo", false);
+            }
 
-        ReportController report = new ReportController(parameters, reportName);
-        report.GeneratePDF();
+            if (ckbSelectVehicle.isSelected()) {
+                parameters.put("sqlFieldExtra", "idVeiculo");
+                parameters.put("sqlValorExtra", cbVehicleHist.getValue().getId());
+            }
+
+            String reportName = "syntheticReport";
+            if (rbAnalyticReport.isSelected()) {
+                reportName = "analyticHistoric";
+                if (ckbGroupByCategory.isSelected())
+                    reportName = "analyticReport";
+            }
+
+            ReportController report = new ReportController(parameters, reportName);
+            report.GeneratePDF();
+        } finally {
+            btnGenerateReport.setDisable(false);
+        }
     };
 
     @Override
@@ -148,16 +168,22 @@ public class MainController implements Initializable {
 
         dtInitialDate.setValue(LocalDate.from(LocalDateTime.now().withDayOfMonth(1)));
         dtFinalDate.setValue(LocalDate.from(LocalDateTime.now()));
+        ckbSinceTheBeginning.setOnAction(actionEnablePeriod);
+        ckbSinceTheBeginning.setSelected(false);
+
+        initializeCbVehicle();
+        cbVehicleHist.setDisable(true);
+        ckbSelectVehicle.setOnAction(actionEnableCbVehicle);
+        ckbSelectVehicle.setSelected(false);
 
         final ToggleGroup toggleGroup = new ToggleGroup();
+        toggleGroup.selectedToggleProperty().addListener(observable ->
+                ckbGroupByCategory.setDisable(rbSimplReport.isSelected()));
         rbSimplReport.setToggleGroup(toggleGroup);
         rbAnalyticReport.setToggleGroup(toggleGroup);
         rbSimplReport.setSelected(true);
 
         btnGenerateReport.setOnAction(actionGenerateReport);
-        btnGenerateHistoric.setOnAction(actionGenerateHistoric);
-
-        initializeCbVehicle();
     }
 
     private void initializeCbVehicle() {
